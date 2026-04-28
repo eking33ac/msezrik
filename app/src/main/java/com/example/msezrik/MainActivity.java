beb98596cc05a0ff45f8810ac2522a1c12ec7eeb
Ezri King <flowingtribe@gmail.com>
2026-04-28 15:43:43 -0500

    Added images for flag, bomb, and blue numbers Updated onDraw to check for status as flag, bomb, and blue numbers and draw appropriate bitmaps Added mouseup, mousedown, and longpress variables Added isbomb variable which gets set to false on all tiles until the bombs are created Added while loop to place the 12 bombs Added countNeighboringBombs method to determine what number/status a tile has Added code to onTouch event to detect if user did a long touch or normal touch Moved status-update code to run only if longTouch is true Added and updated status-update code to check for all tile statuses Added code to ensure if the game is over, user cannot do anything anymore

--- app/src/main/java/com/example/msezrik/MainActivity.java
+++ app/src/main/java/com/example/msezrik/MainActivity.java
@@ -17,6 +17,9 @@ import android.view.MotionEvent;
 
 class MineSweeperView extends View {
     private String tag = "EEK";
+    private long mouseUP=0; // Time mouse click was let go
+    private long mouseDOWN=0; // Time mouse was clicked
+    boolean longPress = false;
     private int touchX;
     private int touchY;
     private final int XBASE=40;
@@ -27,16 +30,36 @@ class MineSweeperView extends View {
     private final int COLS=10;
     private final int NUMBOMBS = 12;
     private final int TILE = 10;
-    private final int EMPTY = 11;
+    private final int EMPTY = 0;
     private final int FLAG = 12;
     private final int BOMB = 13;
+    private final int BLUE1 = 1;
+    private final int BLUE2 = 2;
+    private final int BLUE3 = 3;
+    private final int BLUE4 = 4;
+    private final int BLUE5 = 5;
+    private final int BLUE6 = 6;
+    private final int BLUE7 = 7;
+    private final int BLUE8 = 8;
+
     private int flags_remaining = NUMBOMBS;
     private boolean game_over = false;
     Context activityContext;
     Canvas canvas;
     private int status[][];
+    boolean isbomb[][];
     Bitmap empty_image;
     Bitmap tile_image;
+    Bitmap flag_image;
+    Bitmap bomb_image;
+    Bitmap blue1_image;
+    Bitmap blue2_image;
+    Bitmap blue3_image;
+    Bitmap blue4_image;
+    Bitmap blue5_image;
+    Bitmap blue6_image;
+    Bitmap blue7_image;
+    Bitmap blue8_image;
     String str="";
     private class RowCol {
         public int row;
@@ -44,23 +67,53 @@ class MineSweeperView extends View {
     }
     private void print(String s)
     {
-        Log.i(tag, s);
-        //Toast.makeText(activityContext, s, 1).show();
+        // Log.i(tag, s);
+        Toast.makeText(activityContext, s, Toast.LENGTH_LONG).show();
     }
     public MineSweeperView(Context context) {
         super(context);
         activityContext = context;
-        print("MineSweeperView constructor:");
+        // print("MineSweeperView constructor:");
         status = new int[ROWS][COLS];
+        isbomb = new boolean[ROWS][COLS];
+
+        // Set all tiles to TILE and set all isbomb to false
         for (int i=0; i<ROWS; i++) {
             for (int j=0; j<COLS; j++) {
                 status[i][j] = TILE;
+                isbomb[i][j] = false; // all should be initialized to false without this, so this is included for clairity and precaution
             }
         }
+
+        // Place the 12 bombs
+        int count = 0;
+        while (count < NUMBOMBS) {
+            int r = (int) (ROWS * Math.random());
+            int c = (int) (COLS * Math.random());
+            if (!isbomb[r][c]) {
+                // COMMENT BELOW OUT
+                // print("bomb placed at [" + r + "][" + c + "]");
+                isbomb[r][c] = true;
+                count++;
+            }
+        }
+
         // just for wk4 dev step
-        status[0][0] = EMPTY;
+        // status[0][0] = EMPTY;
+
+        // create all tile-version images
         empty_image = BitmapFactory.decodeResource(getResources(), R.drawable.empty);
         tile_image = BitmapFactory.decodeResource(getResources(), R.drawable.tile);
+        flag_image = BitmapFactory.decodeResource(getResources(), R.drawable.flag);
+        bomb_image = BitmapFactory.decodeResource(getResources(), R.drawable.bomb);
+        blue1_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum1);
+        blue2_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum2);
+        blue3_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum3);
+        blue4_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum4);
+        blue5_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum5);
+        blue6_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum6);
+        blue7_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum7);
+        blue8_image = BitmapFactory.decodeResource(getResources(), R.drawable.bluenum8);
     }
     private void drawbmp(Bitmap bm, int x, int y, int w, int h)
     {
@@ -72,24 +125,44 @@ class MineSweeperView extends View {
         int w = getWidth();
         int h = getHeight();
         this.canvas = canvas;
-        print("onDraw: w = " + w + ", h = " + h);
+        // print("onDraw: w = " + w + ", h = " + h);
         canvas.drawColor(Color.WHITE);
         fg.setColor(Color.BLACK);
-        // <SNIP!>
 
+        // DRAW BOARD
         for (int i=0; i<ROWS; i++) {
             for (int j=0; j<COLS; j++) {
                 int xlocat = XBASE+BW*i;
                 int ylocat = YBASE+BH*j;
                 if (status[i][j] == TILE) {
                     drawbmp(tile_image, xlocat, ylocat, BW, BH);
-
                 } else if (status[i][j] == EMPTY) {
                     drawbmp(empty_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BOMB) {
+                    drawbmp(bomb_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == FLAG) {
+                    drawbmp(flag_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE1) {
+                    drawbmp(blue1_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE2) {
+                    drawbmp(blue2_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE3) {
+                    drawbmp(blue3_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE4) {
+                    drawbmp(blue4_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE5) {
+                    drawbmp(blue5_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE6) {
+                    drawbmp(blue6_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE7) {
+                    drawbmp(blue7_image, xlocat, ylocat, BW, BH);
+                } else if (status[i][j] == BLUE8) {
+                    drawbmp(blue8_image, xlocat, ylocat, BW, BH);
                 }
             }
         }
-        // Game Board Border
+
+        // DRAW Game Board Border
         int dfb = 2;    // distance of border from board
         int leftX = XBASE-dfb;
         int rightX = XBASE+BW*COLS+dfb; // should it be +(dfb-1)? It looks like 1px may be included by default because of the line or something.
@@ -134,37 +207,92 @@ class MineSweeperView extends View {
             rc.col = (x - XBASE) / BW;
         }
 
-        print("getIndex: r = " + rc.row + ", c = " + rc.col);
+        // print("getIndex: r = " + rc.row + ", c = " + rc.col);
         return rc;
+    }
+    private int countNeighboringBombs(int r, int c)
+    {
+        int count = 0;
+        final int C = COLS - 1;
+        final int R = ROWS - 1;
+        if ((r > 0) && (c > 0) && (isbomb[r - 1][c - 1])) count++;
+        if ((r > 0) && (isbomb[r - 1][c])) count++;
+        if ((r > 0) && (c < C) && (isbomb[r - 1][c + 1])) count++;
+        if ((c > 0) && (isbomb[r][c - 1])) count++;
+        if ((c < C) && (isbomb[r][c + 1])) count++;
+        if ((r < R) && (c > 0) && (isbomb[r + 1][c - 1])) count++;
+        if ((r < R) && (isbomb[r + 1][c])) count++;
+        if ((r < R) && (c < C) && (isbomb[r + 1][c + 1])) count++;
+        return count;
     }
     @Override
     public boolean onTouchEvent(MotionEvent e)
     {
+        // don't do anything if game is over
+        if (game_over) { return true; } // Must return SOMETHING, so I set it to true like at the end of the method
         touchX = (int) e.getX();
         touchY = (int) e.getY();
-        print("onTouchEvent: x = " + touchX + ", y = " + touchY);
-        RowCol rc;
-        rc = getIndex(touchX, touchY);
-        // validate row and col? and update status array // (set -1)
-        // <snip>
-        // if not invalid, update status array
-        if (!(rc.row == -1 || rc.col == -1)) {
-            int xlocat = XBASE+BW*rc.col;
-            int ylocat = YBASE+BH*rc.row;
-            // update TILE to EMPTY
-            if (status[rc.col][rc.row] == TILE) {
-                status[rc.col][rc.row] = EMPTY;
-            } // Update EMPTY to TILE
-            else if (status[rc.col][rc.row] == EMPTY) {
-                status[rc.col][rc.row] = TILE;
+        // print("onTouchEvent: x = " + touchX + ", y = " + touchY);
+
+        // Detect Longtouch event
+        int action = e.getAction();
+        if(action == MotionEvent.ACTION_DOWN) {
+            mouseDOWN = System.currentTimeMillis();
+        } else if (action == MotionEvent.ACTION_UP) { // all ontouch info goes in here because otherwise it runs onMouseDown
+            mouseUP = System.currentTimeMillis();
+            if (mouseUP - mouseDOWN > 200) {
+                // print("LONG PRESS!");
+                longPress = true;
+            } else {
+                longPress = false;
             }
-        } // else, do not update status array
 
-        invalidate();
-
-
-        return super.onTouchEvent(e);
+            RowCol rc;
+            rc = getIndex(touchX, touchY);
+            // validate row and col? and update status array // (set -1)
+            // <snip>
+            // if place user clicked is on the board (not invalid) update status array
+            if (!(rc.row == -1 || rc.col == -1)) {
+                int xlocat = XBASE + BW * rc.col;
+                int ylocat = YBASE + BH * rc.row;
+                // If a short/normal click and status is TILE
+                if (!longPress && status[rc.col][rc.row] == TILE) {
+                    // If there is a bomb at that row-col index
+                    if (isbomb[rc.col][rc.row]) {
+                        // set status to comb
+                        status[rc.col][rc.row] = BOMB;
+                        // toast GAME OVER
+                        print("Game Over");
+                        // set game_over to true
+                        game_over = true;
+                    } else {
+                        // count neighboring bombs and assign that to the status array at this row-col index
+                        status[rc.col][rc.row] = countNeighboringBombs(rc.col, rc.row);
+                    }
+                } // if longpress is true
+                else if (longPress) {
+                    // if status is FLAG
+                    if (status[rc.col][rc.row] == FLAG) {
+                        // Set status to TILE
+                        status[rc.col][rc.row] = TILE;
+                    } // if status is TILE
+                    else if (status[rc.col][rc.row] == TILE) {
+                        // set status to FLAG
+                        status[rc.col][rc.row] = FLAG;
+                    }
+                }
+            }
+            // Update EMPTY to TILE
+            /* else if (status[rc.col][rc.row] == EMPTY) {
+                status[rc.col][rc.row] = TILE;
+            } */
+            // Update the screen
+            invalidate();
+        }
+        // return super.onTouchEvent(e);
+        return true;
     }
+
 }
 
 
